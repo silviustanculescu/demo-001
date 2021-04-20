068906c07788c25b523ae53a45831179569c2dd0
silviustanculescu <silviu.stanculescu@gmail.com>
2021-04-21 00:39:27 +0300

    4rd commit

--- src/test/java/ro/qualitest/service/ArithmeticExpressionServiceImplTest.java
+++ src/test/java/ro/qualitest/service/ArithmeticExpressionServiceImplTest.java
@@ -23,7 +23,7 @@ public class ArithmeticExpressionServiceImplTest {
     private OperationSpliterService spliterService;
 
     @Test
-    public void test01() {
+    public void testCalculate_Add2Tokens() {
 
         when(spliterService.split("1+1")).thenReturn(Operation.builder()
                 .left(1).op(EOperationType.ADD).right("1").build());
@@ -33,6 +33,38 @@ public class ArithmeticExpressionServiceImplTest {
         final BigDecimal actualResult = arithmeticExpression.calculate("1+1");
 
         assertEquals(2, actualResult.intValue());
+    }
+
+    @Test
+    public void testCalculate_Add3Tokens() {
+
+        when(spliterService.split("1+1+7")).thenReturn(Operation.builder()
+                .left(1).op(EOperationType.ADD).right("1+7").build());
+        when(spliterService.split("1+7")).thenReturn(Operation.builder()
+                .left(1).op(EOperationType.ADD).right("7").build());
+        when(spliterService.split("7")).thenReturn(Operation.builder()
+                .left(7).build());
+
+        final BigDecimal actualResult = arithmeticExpression.calculate("1+1+7");
+
+        assertEquals(9, actualResult.intValue());
+    }
+
+    @Test
+    public void testCalculate_Add_Substract4Tokens() {
+
+        when(spliterService.split("8+1-5+7")).thenReturn(Operation.builder()
+                .left(8).op(EOperationType.ADD).right("1-5+7").build());
+        when(spliterService.split("1-5+7")).thenReturn(Operation.builder()
+                .left(1).op(EOperationType.SUBSTRACT).right("5+7").build());
+        when(spliterService.split("5+7")).thenReturn(Operation.builder()
+                .left(5).op(EOperationType.ADD).right("7").build());
+        when(spliterService.split("7")).thenReturn(Operation.builder()
+                .left(7).build());
+
+        final BigDecimal actualResult = arithmeticExpression.calculate("8+1-5+7");
+
+        assertEquals(11, actualResult.intValue());
     }
 
 }
